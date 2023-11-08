ed95184fb8861633930f87d837161f680add08a2
juliadaser <bessj006@newschool.edu>
2023-11-08 16:22:41 -0500

    hello

---
+++ test.js
@@ -0,0 +1,18 @@
+// Import the express library and assign it to a variable
+import express from "express";
+
+// Create an instance of an express application
+const app = express();
+
+// Set the port the application will be running on
+const port = process.env.PORT || 3001;
+
+// Set up a response for the root path of the application
+app.get("/", (req, res) => {
+  res.send("HELLO this is node speaking draw a node on a tree");
+});
+
+// Set the application to listen a port
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
